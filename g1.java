7ac88d72e8cf1fe01e5828efbf948217ce53ed5b
Naveenb2599 <47916354+Naveenb2599@users.noreply.github.com>
2019-02-24 14:53:41 +0530

    Create g1.java

---
+++ g1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class g1{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a>0)
+        {
+            System.out.print("Positive");
+        }
+        else if(a<0)
+        {
+            System.out.print("Negative");
+        }else
+        {
+            System.out.print("Zero");
+        }
+    }
+}
