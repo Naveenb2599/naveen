b933255d7da40b47b27860461e8efdd2f1c60069
Naveenb2599 <47916354+Naveenb2599@users.noreply.github.com>
2019-02-25 11:49:48 +0530

    Create g8.java

---
+++ g8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class g8{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int sum=0;
+        for(int i=1;i<=a;i++)
+        {
+            sum=sum+i;
+        }
+            System.out.print(sum);
+}}
