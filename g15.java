078738ec56183797a1be5992bc9be3312824e01b
Naveenb2599 <47916354+Naveenb2599@users.noreply.github.com>
2019-02-25 14:46:09 +0530

    Create g15.java

---
+++ g15.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class g15{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+        for(int i=n;i<m;i++)
+        {
+            if(i==1)
+            {
+            }
+        else if((i%2)==0)
+        {
+            System.out.print(i+" ");
+}
+}}}
